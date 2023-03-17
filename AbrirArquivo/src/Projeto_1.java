adf851e707fefc4d59f523c9fe939a70564ba3d9
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2023-03-17 16:36:30 -0300

    17 03

---
+++ AbrirArquivo/src/Projeto_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_1;
+
+/**
+ *
+ * @author a2104679
+ */
+public class Projeto_1 {
+    
+    public static void main(String[] args) {
+        LeitorArquivo ldat = new LeitorArquivo("programa1.gyh");
+        int c;
+
+        System.out.println("teste: " + args[0]);
+
+    }
+    
+}
--- AbrirArquivo/src/TipoToken.java
+++ AbrirArquivo/src/TipoToken.java
@@ -1,7 +1,8 @@
+package projeto_1;
 
 public enum TipoToken {
-	PCDec, PCProg, PCint, PCReal, PCLer, PCImprimir, PCSe, PCEntao, PCEngto, PCIni, PCFim, OpAritMult, OpAritDiv,
-	OpAritSoma, OpAritSub, OpRelMenor, OpRelMenorigual, OpRelMaior, OpRelMaiorigual, OpRellgual, OpRelDif, OpBoolE,
-	OpBoolOu, Delim, Atrib, AbrePar, FechaPar, Var, Numint, NumReal, Cadeia
-
+    PCDec, PCProg, PCint, PCReal, PCLer, PCImprimir, PCSe, PCEntao, PCEnqto, PCIni, PCFim, OpAritMult, OpAritDiv,
+    OpAritSoma, OpAritSub, OpRelMenor, OpRelMenorigual, OpRelMaior, OpRelMaiorigual, OpRellgual, OpRelDif, OpBoolE,
+    OpBoolOu, Delim, Atrib, AbrePar, FechaPar, Var, Numint, NumReal, Cadeia,
+    Error
 }
