f4cdab6e2be183610ffc572b4f40a03ac6294c02
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2023-03-23 20:03:15 -0300

    Add files via upload

---
+++ AbrirArquivo/src/projeto/LeitorArquivo.java
@@ -0,0 +1,30 @@
+package projeto;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LeitorArquivo {
+    public InputStream is;
+
+    public LeitorArquivo(String name) {
+        try {
+            is = new FileInputStream(name);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public int proxCaractere() {
+        int c=-1;
+        try {
+            c = is.read();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return (c);
+    }
+}
---
+++ AbrirArquivo/src/projeto/TipoToken.java
@@ -0,0 +1,8 @@
+package projeto;
+
+public enum TipoToken {
+    PCDec, PCProg, PCint, PCReal, PCLer, PCImprimir, PCSe, PCEntao, PCEnqto, PCIni, PCFim, OpAritMult, OpAritDiv,
+    OpAritSoma, OpAritSub, OpRelMenor, OpRelMenorigual, OpRelMaior, OpRelMaiorigual, OpRellgual, OpRelDif, OpBoolE,
+    OpBoolOu, Delim, Atrib, AbrePar, FechaPar, Var, Numint, NumReal, Cadeia,
+    Error
+}
