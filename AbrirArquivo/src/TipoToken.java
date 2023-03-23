61b106bb4266dbc716eff4967820424745a0bcb3
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2023-03-23 20:02:28 -0300

    Delete AbrirArquivo/src directory

--- AbrirArquivo/src/TipoToken.java
+++
@@ -1,8 +0,0 @@
-package projeto_1;
-
-public enum TipoToken {
-    PCDec, PCProg, PCint, PCReal, PCLer, PCImprimir, PCSe, PCEntao, PCEnqto, PCIni, PCFim, OpAritMult, OpAritDiv,
-    OpAritSoma, OpAritSub, OpRelMenor, OpRelMenorigual, OpRelMaior, OpRelMaiorigual, OpRellgual, OpRelDif, OpBoolE,
-    OpBoolOu, Delim, Atrib, AbrePar, FechaPar, Var, Numint, NumReal, Cadeia,
-    Error
-}
