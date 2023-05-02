f4e64b7ee07ba207ae869414f4561c4b72c73534
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2023-05-02 16:26:16 -0300

    99% done

--- MainLexicoFazerSintatico/src/main/Sintatico.java
+++ MainLexicoFazerSintatico/src/main/Sintatico.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sintatico {
+
     LeitorArquivo dataRead;
-	List<Token> listaTokens;
-	int index;
-	int erroEncontrado;
-	
-	public Sintatico(String arquivoUtilizado, List inputListaTokens){
-		try {
+    List<Token> listaTokens;
+    int index;
+    int erroEncontrado;
+
+    public Sintatico(String arquivoUtilizado, List inputListaTokens) {
+        try {
             this.dataRead = new LeitorArquivo(arquivoUtilizado);
-		} catch (Exception e) {
+        } catch (Exception e) {
             System.out.print("File not found;\n");
             throw e;
         }
-		
-		this.listaTokens = inputListaTokens;
-		
-		index = 0;
-		erroEncontrado = 0;
-	}
-	
-	/*
+
+        this.listaTokens = inputListaTokens;
+
+        index = 0;
+        erroEncontrado = 0;
+    }
+
+    /*
 			Programa → ':' 'DEC' ListaDeclaracoes ':' 'PROG' ListaComandos;
 		ListaDeclaracoes → Declaracao ListaDeclaracoes2;
 		ListaDeclaracoes2 → ListaDeclaracoes | e;
@@ -52,180 +53,352 @@ public class Sintatico {
 	ComandoCondicao2 → 'SENAO' Comando | e;
 	ComandoRepeticao → 'ENQTO' ExpressaoRelacional Comando;
 	SubAlgoritmo → 'INI' ListaComandos 'FIM';
-	*/
-	
-	// ver notepadd++ /\
-	
-	public void verificadorSintatico(){
-		System.out.print("\n\nQuantidade de tokens: " + listaTokens.size() +
-				"\nUltimo token: " + listaTokens.get(listaTokens.size() - 1).lexema +
-				"\nPrimeiro token: " + listaTokens.get(index).lexema + "\n");
-		
-		for (Token i: listaTokens){
-			if (i.tipo == tipoToken.Error){
-				System.out.println("\n======\nExiste um Token Erro na lista! Sintatico nao sera executado.\n======\n");
-				return;
-			}
-		}
-		
-		this.programa();
-		
-		switch (erroEncontrado){
-			case 1:
-				System.out.println("\nErro sintático: ");
-				break;
-		}
-	}
-	
-	//Programa → ':' 'DEC' ListaDeclaracoes ':' 'PROG' ListaComandos;
-	void programa(){
-		tipoToken.Delim;
-		tipoToken.PCDec;
-		listaDeclaracoes();
-		tipoToken.Delim;
-		tipoToken.PCProg;
-	}
-	
-	//ListaDeclaracoes → Declaracao ListaDeclaracoes2;
-	void listaDeclaracoes(){
-		declaracao();
-		listaDeclaracoes2();
-	}
-	//ListaDeclaracoes2 → ListaDeclaracoes | e;
-	void listaDeclaracoes2(){
-		listaDeclaracoes();
-		//ou
-		empty;
-	}
-	
-	//Declaracao → VARIAVEL ':' TipoVar;
-	void declaracao(){
-		tipoToken.Var;
-		tipoToken.Delim;
-		tipoVar();
-	}
-	
-	//TipoVar → 'INT' | 'REAL';
-	void tipoVar(){
-		tipoToken.NumInt;
-		//ou
-		tipoToken.NumReal;
-	}
+     */
+    // ver notepadd++ /\
+    public void verificadorSintatico() {
+        System.out.print("\n\nQuantidade de tokens: " + listaTokens.size()
+                + "\nUltimo token: " + listaTokens.get(listaTokens.size() - 1).lexema
+                + "\nPrimeiro token: " + listaTokens.get(index).lexema + "\n");
 
-	//ExpressaoAritmetica → TermoAritmetico ExpressaoAritmetica2;
-	void expressaoAritmetica(){
-		termoAritmetico();
-		expressaoAritmetica2();
-	}
-	//ExpressaoAritmetica2 → '+' ExpressaoAritmetica | '-' ExpressaoAritmetica | e;
-	void expressaoAritmetica2(){
-		"+"{
-			expressaoAritmetica();
-		}
-		//ou
-		"-"{
-			expressaoAritmetica();
-		}
-		//ou
-		empty;
-	}
-	
-	//TermoAritmetico → FatorAritmetico TermoAritmetico2;
-	void TermoAritmetico(){
-		fatorAritmetico();
-		termoAritmetico2();
-	}
-	//TermoAritmetico2 → '*' FatorAritmetico TermoAritmetico2 | '/' FatorAritmetico TermoAritmetico2 | e;
-	void termoAritmetico2(){
-		tipoToken.OpAritMult{
-			fatorAritmetico();
-			termoAritmetico2();
-		}
-		//ou
-		tipoToken.OpAritDiv{
-			fatorAritmetico();
-			termoAritmetico2();
-		}
-		//ou
-		empty;
-	}
-	
-	FatorAritmetico → NUMINT | NUMREAL | VARIAVEL | '(' ExpressaoAritmetica ')'
-	void fatorAritmetico(){
-		
-	}
-	
-	ExpressaoRelacional → TermoRelacional ExpressaoRelacional2;
-	void ExpressaoRelacional(){
-		
-	}
-	ExpressaoRelacional2 → OperadorBooleano TermoRelacional ExpressaoRelacional2 | e;
-	void ExpressaoRelacional2(){
-		
-	}
-	
-	TermoRelacional → ExpressaoAritmetica OP_REL ExpressaoAritmetica | '(' ExpressaoRelacional ')';
-	void TermoRelacional(){
-		
-	}
-	
-	OperadorBooleano → 'E' | 'OU';
-	void OperadorBooleano(){
-		
-	}
+        for (Token i : listaTokens) {
+            if (i.tipo == tipoToken.Error) {
+                System.out.println("\n======\nExiste um Token Erro na lista! Sintatico nao sera executado.\n======\n");
+                return;
+            }
+        }
 
-	ListaComandos → Comando ListaComandos2;
-	void ListaComandos(){
-		
-	}
-	ListaComandos2 → ListaComandos | e;
-	void ListaComandos2(){
-		
-	}
+        this.programa();
 
-	
-	
-	//########################################
-		Comando → ComandoAtribuicao | ComandoEntrada | ComandoSaida | ComandoCondicao | ComandoRepeticao | SubAlgoritmo;
-		void Comando(){
+        switch (erroEncontrado) {
+            case 1:
+                System.out.println("\nErro sintático: ");
+                break;
+        }
+    }
 
-		}
-		
-	ComandoAtribuicao → VARIAVEL ':=' ExpressaoAritmetica;
-	void ComandoAtribuicao(){
-		
-	}
-	
-	ComandoEntrada → 'LER' VARIAVEL;
-	void ComandoEntrada(){
-		
-	}
-	
-	ComandoSaida → 'IMPRIMIR' ComandoSaida2;
-	void ComandoSaida(){
-		
-	}
-	ComandoSaida2 → VARIAVEL | CADEIA;
-	void ComandoSaida2(){
-		
-	}
-	
-	ComandoCondicao → 'SE' ExpressaoRelacional 'ENTAO' Comando ComandoCondicao2;
-	void ComandoCondicao(){
-		
-	}
-	ComandoCondicao2 → 'SENAO' Comando | e;
-	void ComandoCondicao2(){
-		
-	}
-	
-	ComandoRepeticao → 'ENQTO' ExpressaoRelacional Comando;
-	void ComandoRepeticao(){
-		
-	}
-	
-	SubAlgoritmo → 'INI' ListaComandos 'FIM';
-	void SubAlgoritmo(){
-		
-	}
+    //Programa → ':' 'DEC' ListaDeclaracoes ':' 'PROG' ListaComandos;
+    void programa() {
+        if (listaTokens.get(index).tipo == tipoToken.Delim)
+            index++;
+        else
+            System.exit(2);
+        
+        if (listaTokens.get(index).tipo == tipoToken.PCDec)
+            index++;
+        else
+            System.exit(2);
+        
+        listaDeclaracoes();
+        
+        if (listaTokens.get(index).tipo == tipoToken.Delim)
+            index++;
+        else
+            System.exit(2);
+            
+        if (listaTokens.get(index).tipo == tipoToken.PCProg)
+            index++;
+        else
+            System.exit(2);
+        
+        System.out.println("\nSintatico verificado completamente!!\n");
+    }
+
+    //ListaDeclaracoes → Declaracao ListaDeclaracoes2;
+    void listaDeclaracoes() {
+        declaracao();
+        listaDeclaracoes2();
+    }
+    
+    //ListaDeclaracoes2 → ListaDeclaracoes | e;
+    void listaDeclaracoes2() {
+        if (listaTokens.get(index).tipo == tipoToken.Var)
+            listaDeclaracoes();
+    }
+
+    //Declaracao → VARIAVEL ':' TipoVar;
+    void declaracao() {
+        if (listaTokens.get(index).tipo == tipoToken.Var)
+            index++;
+        else 
+            System.exit(2);
+        
+        if (listaTokens.get(index).tipo == tipoToken.Delim)
+            index++;
+        else 
+            System.exit(2);
+        
+        tipoVar();
+    }
+
+    //TipoVar → 'INT' | 'REAL';
+    void tipoVar() {
+        if (listaTokens.get(index).tipo == tipoToken.NumInt)
+            index++;
+        else if (listaTokens.get(index).tipo == tipoToken.NumReal)
+            index++;
+        else 
+            System.exit(2);
+    }
+
+    //ExpressaoAritmetica → TermoAritmetico ExpressaoAritmetica2;
+    void expressaoAritmetica() {
+        termoAritmetico();
+        expressaoAritmetica2();
+    }
+    //ExpressaoAritmetica2 → '+' ExpressaoAritmetica | '-' ExpressaoAritmetica | e;
+
+    void expressaoAritmetica2() {
+        switch(listaTokens.get(index).tipo){
+            case OpAritSoma:
+            case OpAritSub:
+                expressaoAritmetica();
+                index++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    //TermoAritmetico → FatorAritmetico TermoAritmetico2;
+    void termoAritmetico() {
+        fatorAritmetico();
+        termoAritmetico2();
+    }
+    //TermoAritmetico2 → '*' FatorAritmetico TermoAritmetico2 | '/' FatorAritmetico TermoAritmetico2 | e;
+
+    void termoAritmetico2() {
+        switch(listaTokens.get(index).tipo){
+            case OpAritMult:
+            case OpAritDiv:
+                fatorAritmetico();
+                termoAritmetico2();
+                index++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    //FatorAritmetico → NUMINT | NUMREAL | VARIAVEL | '(' ExpressaoAritmetica ')'
+    void fatorAritmetico() {
+        switch(listaTokens.get(index).tipo){
+            case NumInt:
+            case NumReal:
+            case Var:
+                index++;
+                break;
+            case AbrePar:
+                index++;
+                expressaoAritmetica();
+                
+                if (listaTokens.get(index).tipo == tipoToken.FechaPar)
+                    index++;
+                else 
+                    System.exit(2);
+   
+                break;
+            default:
+                System.exit(2);
+        }
+    }
+
+    //ExpressaoRelacional → TermoRelacional ExpressaoRelacional2;
+    void expressaoRelacional() {
+        termoRelacional();
+        expressaoRelacional2();
+    }
+    
+    //ExpressaoRelacional2 → OperadorBooleano TermoRelacional ExpressaoRelacional2 | e;
+    void expressaoRelacional2() {
+        if (listaTokens.get(index).tipo == tipoToken.OPBoolE
+         || listaTokens.get(index).tipo == tipoToken.OPBoolOu){
+            operadorBooleano();
+            termoRelacional();
+            expressaoRelacional2();
+        }
+    }
+
+    //TermoRelacional → ExpressaoAritmetica OP_REL ExpressaoAritmetica | '(' ExpressaoRelacional ')';
+    void termoRelacional() {
+        
+        ??????????
+//        switch(listaTokens.get(index).tipo){
+//            case NumInt:
+//            case NumReal:
+//            case Var:
+//            case AbrePar: 
+//                expressaoAritmetica(){
+//                    switch (op  relacionais);
+//                    expressaoAritmetica();
+//                }
+//                //ou
+//                expressaoRelacional();
+//                
+//                break;
+//            
+//        }
+//        //ou
+//        tipoToken.AbrePar{
+//                
+//            tipoToken.FechaPar;
+//        }
+    }
+        
+    //OperadorBooleano → 'E' | 'OU';
+    void operadorBooleano(){
+        switch(listaTokens.get(index).tipo){
+            case OPBoolE:
+            case OPBoolOu:
+                index++;
+                break;
+            default:
+                System.exit(2);
+        }
+    }
+
+    //ListaComandos → Comando ListaComandos2;
+    void listaComandos(){
+        comando();
+        listaComandos2();
+    }
+    //ListaComandos2 → ListaComandos | e;
+    void listaComandos2(){
+        switch(listaTokens.get(index).tipo){
+            case Var:
+            case PCLer:
+            case PCSe:
+            case PCImprimir:
+            case PCEnqto:
+            case PCIni:
+                listaComandos();
+            default:
+                break;
+        }
+    }
+
+
+
+    //########################################
+    //Comando → ComandoAtribuicao | ComandoEntrada | ComandoSaida | ComandoCondicao | ComandoRepeticao | SubAlgoritmo;
+    void comando(){
+        tipoToken aux = listaTokens.get(index).tipo;
+        
+        if (aux == tipoToken.Var)
+            comandoAtribuicao();
+        else if (aux == tipoToken.PCLer)
+            comandoEntrada();
+        else if (aux == tipoToken.PCImprimir)
+            comandoSaida();
+        else if (aux == tipoToken.PCSe)
+            comandoCondicao();
+        else if (aux == tipoToken.PCEnqto)
+            comandoRepeticao();
+        else if (aux == tipoToken.PCIni)
+            subAlgoritmo();
+        else
+            System.exit(2);
+    }
+
+    //ComandoAtribuicao → VARIAVEL ':=' ExpressaoAritmetica;
+    void comandoAtribuicao(){
+        if (listaTokens.get(index).tipo == tipoToken.Var)
+            index++;
+        else 
+            System.exit(2);
+        
+        if (listaTokens.get(index).tipo == tipoToken.Atrib)
+            index++;
+        else 
+            System.exit(2);
+        
+        expressaoAritmetica();
+    }
+
+    //ComandoEntrada → 'LER' VARIAVEL;
+    void comandoEntrada(){
+        if (listaTokens.get(index).tipo == tipoToken.PCLer)
+            index++;
+        else 
+            System.exit(2);
+        
+        if (listaTokens.get(index).tipo == tipoToken.Var)
+            index++;
+        else 
+            System.exit(2);
+    }
+
+    //ComandoSaida → 'IMPRIMIR' ComandoSaida2;
+    void comandoSaida(){
+        if (listaTokens.get(index).tipo == tipoToken.PCImprimir)
+            index++;
+        else 
+            System.exit(2);
+        
+        comandoSaida2();
+    }
+    //ComandoSaida2 → VARIAVEL | CADEIA;
+    void comandoSaida2(){
+        switch (listaTokens.get(index).tipo){
+            case Var:
+            case Cadeia:
+                index++;
+                break;
+            default:
+                System.exit(2);
+        }
+    }
+
+    //ComandoCondicao → 'SE' ExpressaoRelacional 'ENTAO' Comando ComandoCondicao2;
+    void comandoCondicao(){
+        if (listaTokens.get(index).tipo == tipoToken.PCSe)
+            index++;
+        else 
+            System.exit(2);
+        
+        expressaoRelacional();
+        
+        if (listaTokens.get(index).tipo == tipoToken.PCEntao)
+            index++;
+        else 
+            System.exit(2);
+        
+        comando();
+        comandoCondicao2();
+    }
+    //ComandoCondicao2 → 'SENAO' Comando | e;
+    void comandoCondicao2(){
+        switch(listaTokens.get(index).tipo){
+            case PCSenao:
+                index++;
+                comando();
+            default:
+                break;
+        }
+    }
+
+    //ComandoRepeticao → 'ENQTO' ExpressaoRelacional Comando;
+    void comandoRepeticao(){
+        if (listaTokens.get(index).tipo == tipoToken.PCEnqto)
+            index++;
+        else 
+            System.exit(2);
+        
+        expressaoRelacional();
+        comando();
+    }
+
+    //SubAlgoritmo → 'INI' ListaComandos 'FIM';
+    void subAlgoritmo(){
+        if (listaTokens.get(index).tipo == tipoToken.PCIni)
+            index++;
+        else 
+            System.exit(2);
+        
+        listaComandos();
+        
+        if (listaTokens.get(index).tipo == tipoToken.PCFim)
+            index++;
+        else 
+            System.exit(2);
+    }
 }
--- MainLexicoFazerSintatico/src/main/tipoToken.java
+++ MainLexicoFazerSintatico/src/main/tipoToken.java
@@ -3,5 +3,5 @@ package main;
 public enum tipoToken {
     OpAritMult,OpAritDiv,OpAritSoma,OpAritSub,AbrePar,FechaPar,OpRelMenorigual,Cadeia,OpRelIgual,Error,
 	OpRelMaior,OpRelMaiorigual,OpRelDif,OPBoolOu,OPBoolE,PCDec,PCProg,PCIni,PCint,PCImprimir,PCLer,PCSe,PCEnqto,PCEntao,
-	Delim,Atrib,PCFim,Var,NumInt,NumReal,PCReal,OpRelMenor
+	Delim,Atrib,PCFim,Var,NumInt,NumReal,PCReal,OpRelMenor,PCSenao
 }
